627922abd2958c0d4020f0d7c2c6d61b46f1a832
Timur <xuser77@gmail.com>
2025-12-01 20:49:21 +0500

    Open position fixes 2

--- contracts/X2UniswapRouter.sol
+++ contracts/X2UniswapRouter.sol
@@ -26,16 +26,12 @@ contract X2UniswapRouter is ISwapRouter {
     }
 
     function swap(address tokenIn, uint256 amountIn, uint256 minAmountOut) external override returns (uint256 amountOut) {
-        console.log("Swap start");
         address[] memory path = _buildPath(tokenIn);
 
-        console.log("Path: ok");
         // pull tokens in
         require(IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn), "Pull failed");
-        console.log("transferFrom passed");
         // approve router if needed
         _ensureApproval(tokenIn, amountIn);
-        console.log("approval passed");
 
         uint256[] memory amounts = IUniV2Router(uniV2Router).swapExactTokensForTokens(
             amountIn,
